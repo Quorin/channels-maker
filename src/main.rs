e1698167cfa22367f493b4801d67688f079d902c
Quorin <xquoris@gmail.com>
2021-07-01 15:04:29 +0200

    feat: Clap integration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate lazy_static;
 
+use clap::{AppSettings, Clap};
+
 use crate::config::Config;
 use crate::error::MakerResult;
 use crate::maker::Maker;
@@ -9,7 +11,15 @@ mod config;
 mod error;
 mod maker;
 
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(short, long, about = "Delete directories and files that are not whitelisted")]
+    force: bool,
+}
+
 fn main() -> MakerResult<()> {
+    let opts: Opts = Opts::parse();
     let config = Config::read_config()?;
     let maker = Maker::new(config)?;
     maker.check_current_directory()?;
